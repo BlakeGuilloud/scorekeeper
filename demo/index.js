8140d3226b985a79ff58be7513f1e8bf76e8e75b
Blake Guilloud <blake.guilloud@gmail.com>
2018-02-25 18:45:17 -0500

    Blake - optional arg for dirname from cli.

--- demo/index.js
+++ demo/index.js
@@ -3,4 +3,4 @@ const scorekeeper = require('../dist');
 const filePath = 'scores.txt';
 const fileDest = 'rank1.txt';
 
-console.log(scorekeeper(__dirname, filePath, fileDest));
+// console.log(scorekeeper(__dirname, filePath, fileDest));
